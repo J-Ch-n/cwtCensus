2dd754e28842cc2ceb5487fb16aa42add09454b0
Joshua <jiashu.chen@berkeley.edu>
2023-10-22 01:38:26 -0700

    added `check_dim`, `check_type`, added custom error messages

--- R/error_checking.R
+++ R/error_checking.R
@@ -1,13 +1,87 @@
 ### Error Checking Functions ###
 
-# Check for the dimension for the input.
-check_dim <- function() {}
+# Check for the dimension for the input. Return empty list if dimensions match, and
+# return the error information for each input. The error list is in the following
+# format: NAME, actual COL_NUM, expected COL_NUM.
+check_dim <- function(rel, reco) {
+  # Recovery Data Frame Column Numbers.
+  RECO_NUM_COL = 11
 
-# Check for the type of the input.
-check_type <-
-  function(rel, reco, size_at_age, rel_mort, nat_mort,
+  #Release Data Frame Column Numbers.
+  REL_NUM_COL = 5
+
+  # Release Data Frame Column Names.
+  REL_COL = c("release_month",
+              "tag_code",
+              "production_expansion_factor",
+              "total_released",
+              "brood_year")
+
+  # Recovery Data Frame Column Names.
+  RECO_COL = c("run_year",
+               "brood_year",
+               "recovery_id",
+               "fisheries",
+               "tag_code",
+               "length_at_age",
+               "sex",
+               "month",
+               "location",
+               "size_limit",
+               "estimated_num")
+
+  error =  list()
+  if (ncol(rel) != REL_NUM_COL) {
+    error[["rel"]] = c("'release'", ncol(rel), REL_NUM_COL)
+  }
+
+  if (ncol(rec) != RECO_NUM_COL) {
+    error[["rec"]] = c("'recoveries'", ncol(rec), RECO_NUM_COL)
+  }
+
+  return(error)
+}
+
+# Check for the type of the input. Upon unexpected data type, return a vector of
+# incorrect argument name and expected data type.
+check_type <- function(rel, reco, size_at_age, rel_mort, nat_mort,
                        sex, fisheries, bootstrap, bootstrap_iter) {
+  error = c()
+  df = "data frame"
+  ch = "character"
+  lg = "logical"
+  nm = "numeric"
 
+  if (!is.data.frame(rel)) {
+    error = c("'release'", df, typeof(rel))
+
+  } else if (!is.data.frame(reco)) {
+    error = c("'recovery'", df, typeof(reco))
+
+  } else if (!is.data.frame(size_at_age)) {
+    error = c("'size_at_age'", df, typeof(size_at_age))
+
+  } else if (!is.data.frame(rel_mort)) {
+    error = c("'release mortality'", df, typeof(rel_mort))
+
+  } else if (!is.data.frame(nat_mort)) {
+    error = c("'natural mortality'", df, typeof(nat_mort))
+
+  } else if (!is.character(sex)) {
+    error = c("'sex'", ch, typeof(sex))
+
+  } else if (!is.data.frame(fisheries)) {
+    error = c("'fisheries'", df, typeof(fisheries))
+
+  } else if (!is.logical(bootstrap)) {
+    error = c("'bootstrap'", lg, typeof(bootstrap))
+
+  } else if (!is.numeric(bootstrap_iter)) {
+    error = c("'boostrap_iter'", nm, typeof(bootstrap_iter))
+
+  }
+
+  return(error)
 }
 
 # Check for the length of the input.
--- R/main.R
+++ R/main.R
@@ -5,20 +5,37 @@
 #' @return list of cohort reconstructions with or without bootstrapping.
 #' @export
 #' @examples
-ch_reco <- function(rel, reco, size_at_age = stored_df,
-                    rel_mort = stored_df, nat_mort = stored_df, sex = "both",
-                    fisheries = stored_df, bootstrap = True, bootstrap_iter = 1000) {
+ch_reco <- function(rel, reco, size_at_age = rec,
+                    rel_mort = rec, nat_mort = rec, sex = "both",
+                    fisheries = rec, bootstrap = TRUE, bootstrap_iter = 1000) {
   # Check missing inputs
   if (missing(rel) | missing(reco)) {
     stop(paste0("One or both of rel and reco are missing.
                 Please supply input for rel and reco"))
   }
-  # Check incorrect input data type
-  if (check_type(rel, reco, size_at_age, rel_mort, nat_mort,
-             sex, fisheries, bootstrap, bootstrap_iter)) {
-    stop(paste0("Wrong type"))
+  # Check incorrect input data type.
+  type_error = check_type(rel, reco, size_at_age, rel_mort, nat_mort, sex,
+                          fisheries, bootstrap, bootstrap_iter)
+  if (!is_empty(type_error)) {
+    stop(paste0("Wrong type in ", type_error[1], ". Expected ", type_error[2],
+                " but got ", "'", type_error[3], "'."))
+  }
+  # Check incorrect column dimension.
+  col_error = check_dim(rel, reco)
+  if (length(col_error) != 0) {
+    msg = ""
+    append_error_col <- function(elem) {
+      msg <<- paste0(msg, "Error in ", elem[1], ", expected ",
+                   elem[3], " columns, but got ",
+                   elem[2], " columns.\n  ")
+    }
+    lapply(col_error, append_error_col);
+    stop(msg)
   }
   # Check incorrect dimensions for rel, reco, size_at_age, rel_mort, nat_mort, sex, fisheries.
+
   # Check uniqueness of rel and reco
   # Check for missing values in all inputs
 }
+
+
