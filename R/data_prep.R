e411dddb1b72d09668b788e2797a83fe96bb4659
Joshua <jiashu.chen@berkeley.edu>
2023-11-11 20:37:08 -0800

    moved all error checking logic into a wrapper function, providing better protection

--- R/data_prep.R
+++ R/data_prep.R
@@ -4,48 +4,48 @@ data_prep <- function(rel, reco, size_at_age, rel_mort, nat_mort,
                       sex, fisheries, bootstrap, iter){
 
 
-BY <- CWT_Releases %>% #BY for each batch
-      select(tag_code, brood_year, Phi)
+  BY <- CWT_Releases %>% #BY for each batch
+        select(tag_code, brood_year, Phi)
 
-release_data <- function(release){
-  release<- release %>%
-    group_by(brood_year) %>%
-    summarise(Individuals_Released = sum(Total_Released))
-  return(release)
-}
+  release_data <- function(release){
+    release<- release %>%
+      group_by(brood_year) %>%
+      summarise(Individuals_Released = sum(Total_Released))
+    return(release)
+  }
 
-escape_to_hat <- function(recoveries){
-  CWT_Hatchery<-CWT_Recoveries %>%
-    filter(fishery ==50) %>%
-    group_by(run_year, brood_year) %>%
-    summarise(Escapement_to_Hatchery = sum(estimated_number/Phi)) %>%
-    mutate(Age = run_year-brood_year)%>%
-    group_by(Age, run_year, brood_year) %>%
-    summarize(Hat = sum(Escapement_to_Hatchery))
-  return(CWT_Hatchery)
-  # of the form
-  #       Age run_year   brood_year   Hat
-  # 1     4     2002       1998       1.01
-  # 2     4     2006       2002       1.04
-}
-escape_to_spawn <- function(recoveries, bootstrap, iter){
+  escape_to_hat <- function(recoveries){
+    CWT_Hatchery<-CWT_Recoveries %>%
+      filter(fishery ==50) %>%
+      group_by(run_year, brood_year) %>%
+      summarise(Escapement_to_Hatchery = sum(estimated_number/Phi)) %>%
+      mutate(Age = run_year-brood_year)%>%
+      group_by(Age, run_year, brood_year) %>%
+      summarize(Hat = sum(Escapement_to_Hatchery))
+    return(CWT_Hatchery)
+    # of the form
+    #       Age run_year   brood_year   Hat
+    # 1     4     2002       1998       1.01
+    # 2     4     2006       2002       1.04
+  }
+  escape_to_spawn <- function(recoveries, bootstrap, iter){
 
-}
+  }
 
-river_harvest <- function(recoveries){
+  river_harvest <- function(recoveries){
 
-}
+  }
 
-ocean_harvest <- function(release, recoveries){
+  ocean_harvest <- function(release, recoveries){
 
-}
-rec_harvest <- function(release, recoveries) {
+  }
+  rec_harvest <- function(release, recoveries) {
 
-}
-com_harvest <- function(release, recoveries){
+  }
+  com_harvest <- function(release, recoveries){
 
-}
-harvest_compute <- function(releases, recoveries, bootstrap, iter){
+  }
+  harvest_compute <- function(releases, recoveries, bootstrap, iter){
 
-}
+  }
 }
--- R/main.R
+++ R/main.R
@@ -7,48 +7,8 @@
 #' @examples
 ch_reco <- function(rel, reco, size_at_age = release, rel_mort = release, nat_mort = release,
                     sex = "both", fisheries = release, bootstrap = TRUE, iter = 1000) {
-# Handles list of errors in ERR of check type TYPE. Aggregate error messages and
-# stops with the composite error message at the end.
-error_handler <- function(err, type) {
-
-  # Appends each error message ELEM to the previous message.
-  # Modifies the parent variable MSG.
-  append_error <- function(elem) {
-    msg <<- paste0(msg, "Error in ", elem[1], ", expected ",
-                   elem[2], " ", type," but got ",
-                   elem[3], " ", type, ".\n  ")
-  }
-  msg = ""
-  lapply(err, append_error)
-  stop(msg)
-}
-
-# Check missing required inputs REL and RECO.
-if (missing(rel) | missing(reco)) {
-  stop(paste0("One or both of rel and reco are missing.
-              Please supply input for rel and reco"))
-}
-
-# Check incorrect input data type.
-typ_err = check_type(rel, reco, size_at_age, rel_mort, nat_mort, sex, fisheries, bootstrap, iter)
-if (length(typ_err) > 0) error_handler(typ_err, "type")
-
-# Check incorrect number of columns.
-col_err = check_dim(rel, reco, size_at_age, rel_mort, nat_mort, fisheries)
-if (length(col_err) > 0) error_handler(col_err, "column(s)")
-
-# Check incorrec† column names of REL and RECO.
-nam_err = check_columns(rel, reco)
-if (length(nam_err) > 0) error_handler(nam_err, "columns")
-
-# Check uniqueness of REL and RECO
-u_col = check_unique(rel, reco)
-rel['tag_code'] = u_col[[1]]
-reco['recovery_id'] = u_col[[2]]
-
-# Check for missing values in all inputs
-check_nan(rel, reco, size_at_age, rel_mort, nat_mort, fisheries)
-
+  check_error(rel, reco, size_at_age, rel_mort, nat_mort,
+              sex, fisheries, bootstrap, iter)
 }
 
 
