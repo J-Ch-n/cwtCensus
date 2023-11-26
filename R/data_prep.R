08f52a9141f21076581c6ec6ea23f1a076f88a04
Joshua <jiashu.chen@berkeley.edu>
2023-11-25 23:25:41 -0800

    updated design doc, created a new branch for the new approach, startec coding, did a test to compare `lazy_dt` and `left_join` performance

--- R/data_prep.R
+++ R/data_prep.R
@@ -1,7 +1,17 @@
 ### Data Preparation Function ###
 
 data_prep <- function(rel, reco, size_at_age, rel_mort, nat_mort,
-                      sex, fisheries, bootstrap, iter) {
+                      sex, ret_spawn_id, ret_hat_id, river_harvest,
+                      ocean_rec, ocean_com, bootstrap, iter) {
 
+  # Mapping between internal fishery id and user provided fishery ids. #
+  spawn = ret_spawn_id
+  hat = ret_hat_id
+  river = river_harvest
+  ocean_c = ocean_com
+  ocean_r = ocean_rec
+
+  # Use data.frame left join (faster than lazy_dt) see `cohort_recon_pk/exploratory_files/data_prep_exploration.R`
+  rel_reco = reco %>% left_join(rel, by = 'tag_code')
 
 }
