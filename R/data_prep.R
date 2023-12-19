54aa431c918692aab1442048cdd814b4485edb17
Joshua <jiashu.chen@berkeley.edu>
2023-12-19 01:03:48 -0800

    added rel_reco_dt; added `age`, `total_individual` columns and group by with native data table syntax

--- R/data_prep.R
+++ R/data_prep.R
@@ -1,17 +1,31 @@
 ### Data Preparation Function ###
 
 data_prep <- function(rel, reco, size_at_age, rel_mort, nat_mort,
-                      sex, ret_spawn_id, ret_hat_id, river_harvest,
-                      ocean_rec, ocean_com, bootstrap, iter) {
+                      sex, spawn, hat, river,
+                      ocean_r, ocean_c, bootstrap, iter) {
+  # Left_join reco and rel and create a data table from the result.
+  rel_reco_dt = reco |>
+    left_join(rel, by = 'tag_code') |>
+    setDT()
 
-  # Mapping between internal fishery id and user provided fishery ids. #
-  spawn = ret_spawn_id
-  hat = ret_hat_id
-  river = river_harvest
-  ocean_c = ocean_com
-  ocean_r = ocean_rec
+  # Create age column.
+  rel_reco_dt[ , age := fcase(
+    month >= birth_month, run_year - brood_year,
+    month < birth_month, run_year - brood_year - 1
+    # Group by BY, Month, Age, Fishery, Location.
+  )][ , .(total_individual = sum(est_num) / prod_exp),
+      by = c('brood_year', 'month', 'age', 'fishery', 'location'),
+  ]
+  # [, c('run_year', 'recovery_id', 'tag_code', 'length', 'sex', 'size_limit', 'release_month', 'prod_exp', 'birth_month') :=
+  #     list(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)]
 
-  # Use data.frame left join (faster than lazy_dt) see `cohort_recon_pk/exploratory_files/data_prep_exploration.R`
-  rel_reco = reco %>% left_join(rel, by = 'tag_code')
-
+  view(rel_reco_dt)
+  #
+  # rel_reco_ldt |>
+  #   mutate(age = case_when(
+  #     month >= birth_month ~ run_year - brood_year,
+  #     TRUE ~ run_year - brood_year - 1
+  #   )) |>
+  #   group_by(brood_year, month, age, fishery, location) |>
+  #   summarize(total_indiv = sum(est_num / prod_exp))
 }
