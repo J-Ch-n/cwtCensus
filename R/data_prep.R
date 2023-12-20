b116fa6aebebfddb196f123a1bd4447de6bbf598
Joshua <jiashu.chen@berkeley.edu>
2023-12-20 13:03:47 -0800

    working on step 2; changed matrices to data table for in place modification; working on aggregation function; added rlang package dependency in DESCRIPTION

--- R/data_prep.R
+++ R/data_prep.R
@@ -1,8 +1,16 @@
 ### Data Preparation Function ###
 
 data_prep <- function(rel, reco, size_at_age, rel_mort, nat_mort,
-                      sex, spawn, hat, river,
+                      sex, spawn, hatchery, river,
                       ocean_r, ocean_c, bootstrap, iter) {
+
+  # Create mappings from column name to column indices.
+  by_idx = 1
+  mth_idx = 2
+  age_idx = 3
+  fshry_idx = 4
+  loc_idx = 5
+  total_idx = 6
 
   ######################
   ### Intermediate 1 ###
@@ -44,12 +52,45 @@ data_prep <- function(rel, reco, size_at_age, rel_mort, nat_mort,
   ######################
 
   # Assume brood years are densely and contiguously distributed.
-  # Declare and zero-fill the 2-dimensional matrix for storing maturation.
-  maturation_matrix = matrix(data = c(0),
-                             nrow = by_range_cnt, ncol = age_range_cnt)
+  # Declare and zero-fill a data table with columns "by", "age", and "maturation"
+  # for storing maturation.
 
-  # Declare and zero-fill the 3-dimensional matrix for storing impact.
-  impact_matrix = array(data = c(0),
-                        dim = c(by_range_cnt, month_range_cnt, age_range_cnt))
+  m_init_vec = rep.int(0, by_range_cnt * age_range_cnt)
 
+  maturation_dt = data.table(by = m_init_vec,
+                             age = m_init_vec,
+                             maturation = m_init_vec)
+
+  # Declare and zero-fill a data table for storing impact and natural mortality.
+  inm_init_vec = rep.int(0, by_range_cnt * age_range_cnt * month_range_cnt)
+
+  impact_nat_mort_dt = data.table(by = inm_init_vec,
+                                  month = inm_init_vec,
+                                  age = inm_init_vec,
+                                  impact = inm_init_vec,
+                                  nat_mort = inm_init_vec)
+  row_idx = 1
+  maturation_temp = 0
+  prev_year = rel_reco_dt[1, ..by_idx]
+  prev_age = rel_reco_dt[1, ..age_idx]
+
+  # Aggregate function for calculating impact, maturation, and natural mortality.
+  find_imp_nat_mat <- function(record) {
+    par_env = env_parent(cur_env())
+
+    prev_year = record[by_idx]
+    prev_age = record[age_idx]
+
+    if (record[fshry_idx] %in% c(spawn, river, hatchery)) {
+      # If fishery is spawning ground, river harvest, or hatchery escapement,
+      # calculate maturation. Aggregate maturation to `maturation_temp` until we encounter
+      #
+      par_env(maturation_dt) |>
+        set(i = row_idx, j = "maturation")
+
+    } else if (record[fshry_id] %in% c(ocean_r, ocean_c)) {
+      # If fishery is recreational or commercial ocean harvest, calculate natural
+      # mortality and impact.
+    }
+  }
 }
