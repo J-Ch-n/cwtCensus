2e3efbbb8af56263019d5ec9ad5b70eef2b6a045
Joshua <jiashu.chen@berkeley.edu>
2023-12-20 00:01:13 -0800

    started to implement step 2; declared and zero-filled maturation and impact matrices; assumed brood year, age, and month are densely and contiguously distributed.

--- R/data_prep.R
+++ R/data_prep.R
@@ -23,22 +23,33 @@ data_prep <- function(rel, reco, size_at_age, rel_mort, nat_mort,
   # Materialize lazy data table.
   rel_reco_dt = as.data.table(rel_reco_ldt)
 
-  # Find count for unique brood year.
-  by_count = uniqueN(rel_reco_dt$brood_year)
+  # Find min and max brood year.
+  by_min = min(rel_reco_dt$brood_year)
+  by_max = max(rel_reco_dt$brood_year)
+  by_range_cnt = by_max - by_min + 1
+
 
   # Find min and max month.
   month_min = min(rel_reco_dt$month)
   month_max = max(rel_reco_dt$month)
+  month_range_cnt = month_max - month_min + 1
 
   # Find min and max age.
   age_min = min(rel_reco_dt$age)
   age_max = max(rel_reco_dt$age)
+  age_range_cnt = age_max - age_min + 1
 
   ######################
   ### Intermediate 2 ###
   ######################
 
+  # Assume brood years are densely and contiguously distributed.
+  # Declare and zero-fill the 2-dimensional matrix for storing maturation.
+  maturation_matrix = matrix(data = c(0),
+                             nrow = by_range_cnt, ncol = age_range_cnt)
 
-
+  # Declare and zero-fill the 3-dimensional matrix for storing impact.
+  impact_matrix = array(data = c(0),
+                        dim = c(by_range_cnt, month_range_cnt, age_range_cnt))
 
 }
