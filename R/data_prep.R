a38242282fb4208faa1764a5bf96fce7ad2b48e1
Joshua <jiashu.chen@berkeley.edu>
2024-01-12 10:43:43 -0800

    fixed maturation caclulationfrom per BY to per BY, AGE

--- R/data_prep.R
+++ R/data_prep.R
@@ -5,13 +5,22 @@ data_prep <- function(rel, reco, size_at_age, rel_mort, nat_mort,
                       ocean_r = 10, ocean_c = 40, bootstrap, iter) {
 
   # Create mappings from column name to column indices.
-  by_idx = 1
-  mth_idx = 2
-  age_idx = 3
-  fshry_idx = 4
-  loc_idx = 5
-  mat_grp_idx = 6
-  total_idx = 7
+  BY_IDX = 1
+  MNTH_IDX = 2
+  AGE_IDX = 3
+  FSHRY_IDX = 4
+  LOC_IDX = 5
+  MAT_GRP_IDX = 6
+  TOTAL_IDX = 7
+
+  # Find the number of unique year - age pairs for a specific fishery in PROVIDED_FISHERIES.
+  # PROVIDED_FISHERIES is passed in as a vector of fisheries.
+  num_unique_year_age <- function(rel_reco_dt, provided_fisheries) {
+    rel_reco_dt |>
+      filter(fishery %in% provided_fisheries) |>
+      distinct(brood_year, age) |>
+      view()
+  }
 
   ######################
   ### Intermediate 1 ###
@@ -32,7 +41,7 @@ data_prep <- function(rel, reco, size_at_age, rel_mort, nat_mort,
     )) |>
     group_by(brood_year, month, age, fishery, location, maturation_grp) |>
     summarize(total_indiv = sum(est_num / prod_exp)) |>
-    arrange(brood_year, maturation_grp, month, age, fishery)
+    arrange(brood_year, maturation_grp, age, fishery)
 
   # Materialize lazy data table.
   rel_reco_dt = as.data.table(rel_reco_ldt)
@@ -52,6 +61,9 @@ data_prep <- function(rel, reco, size_at_age, rel_mort, nat_mort,
   age_min = min(rel_reco_dt$age)
   age_max = max(rel_reco_dt$age)
   age_range_cnt = age_max - age_min + 1
+
+  # yr_ag_cnt = num_unique_year_age(rel_reco_dt, c(spawn, hatchery, river))
+  # print(yr_ag_cnt)
 
   ######################
   ### Intermediate 2 ###
@@ -85,8 +97,8 @@ data_prep <- function(rel, reco, size_at_age, rel_mort, nat_mort,
   # or river, and false otherwise. The value defaults to false.
   prev_year_valid = F
 
-  prev_year = rel_reco_dt[1, ..by_idx] |> unlist()
-  prev_age = rel_reco_dt[1, ..age_idx] |> unlist()
+  prev_year = rel_reco_dt[1, ..BY_IDX] |> unlist()
+  prev_age = rel_reco_dt[1, ..AGE_IDX] |> unlist()
   view(rel_reco_dt)
 
   # Aggregate function for calculating impact, maturation, and natural mortality.
@@ -94,33 +106,35 @@ data_prep <- function(rel, reco, size_at_age, rel_mort, nat_mort,
     #print(record)
     par_env = env_parent(current_env())
 
-    cur_year = record[by_idx] |> as.integer()
-    cur_age = record[age_idx] |> as.integer()
+    cur_year = record[BY_IDX] |> as.integer()
+    cur_age = record[AGE_IDX] |> as.integer()
 
-    cur_fishery = record[fshry_idx] |> as.numeric()
-
-    # if the record marks a changed brood year, push the maturation value onto the data table.
-    if (cur_year != prev_year && prev_year_valid) {
-      par_env$maturation_dt |>
-        set(i = row_idx, j = "maturation", value =
-              par_env$prev_sp_esc +
-              par_env$prev_riv_harv +
-              par_env$prev_hat_esc)
-      par_env$maturation_dt |>
-        set(i = row_idx, j = "by", value = prev_year)
-      par_env$maturation_dt |>
-        set(i = row_idx, j = "age", value = prev_age)
-
-      row_idx <<- row_idx + 1L
-      prev_sp_esc = prev_riv_harv = prev_hat_esc = 0
-      prev_year_valid <<- FALSE
-    }
+    cur_fishery = record[FSHRY_IDX] |> as.numeric()
 
     if (cur_fishery %in% c(spawn, river, hatchery)) {
+      # if the record marks a changed brood year or age, push the maturation value onto the data table.
+      if (((cur_year != prev_year && prev_year_valid)
+           | (cur_year == prev_year && cur_age != prev_age))) {
+        print(record)
+        par_env$maturation_dt |>
+          set(i = row_idx, j = "maturation", value =
+                par_env$prev_sp_esc +
+                par_env$prev_riv_harv +
+                par_env$prev_hat_esc)
+        par_env$maturation_dt |>
+          set(i = row_idx, j = "by", value = prev_year)
+        par_env$maturation_dt |>
+          set(i = row_idx, j = "age", value = prev_age)
+
+        row_idx <<- row_idx + 1L
+        prev_sp_esc = prev_riv_harv = prev_hat_esc = 0
+        prev_year_valid <<- FALSE
+      }
       # If fishery is spawning ground, river harvest, or hatchery escapement,
       # calculate maturation. Aggregate maturation to `maturation_temp` until we encounter
-    #print(c(cur_year, prev_year, prev_sp_esc, prev_riv_harv, prev_hat_esc))
-      total_indiv = record[total_idx] |> as.numeric()
+      # a new year.
+      # print(c(cur_year, prev_year, prev_sp_esc, prev_riv_harv, prev_hat_esc))
+      total_indiv = record[TOTAL_IDX] |> as.numeric()
 
       # Calculate number of individuals for each type of fishery.
       if (cur_fishery == spawn) {
@@ -133,7 +147,7 @@ data_prep <- function(rel, reco, size_at_age, rel_mort, nat_mort,
 
       prev_year_valid <<- TRUE
 
-    } else if (record[fshry_idx] %in% c(ocean_r, ocean_c)) {
+    } else if (record[FSHRY_IDX] %in% c(ocean_r, ocean_c)) {
       # If fishery is recreational or commercial ocean harvest, calculate natural
       # mortality and impact.
     }
@@ -144,6 +158,18 @@ data_prep <- function(rel, reco, size_at_age, rel_mort, nat_mort,
   }
 
   apply(rel_reco_dt, 1, find_imp_nat_mat)
+  # The apply function doesn't have knowledge of the end of the data frame.
+  # Thus, the solution is one off. We need to apply the last row of maturation here.
+  maturation_dt |>
+    set(i = row_idx, j = "maturation", value =
+          prev_sp_esc +
+          prev_riv_harv +
+          prev_hat_esc)
+  maturation_dt |>
+    set(i = row_idx, j = "by", value = prev_year)
+  maturation_dt |>
+    set(i = row_idx, j = "age", value = prev_age)
+
   view(maturation_dt)
   view(rel_reco_dt)
 }
