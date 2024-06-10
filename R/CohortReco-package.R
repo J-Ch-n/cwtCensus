02032bd3d92e6c48ff1b3116bb7adad8e876ae6f
Joshua <jiashu.chen@berkeley.edu>
2024-06-10 13:34:09 -0700

    Added `CohortReco-package.R`

---
+++ R/CohortReco-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr mutate
+#' @importFrom dplyr left_join
+#' @importFrom dplyr select
+#' @importFrom dplyr arrange
+#' @importFrom dplyr case_when
+#' @importFrom dplyr distinct
+#' @importFrom dplyr summarize
+#' @importFrom dplyr group_by
+#' @import dtplyr
+#' @import data.table
+## usethis namespace: end
+NULL
